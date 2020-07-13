997a6112de31822831186644d2ea5dfe2075fe0c
mchudasama <mchudasama@logilite.com>
2020-07-13 16:40:51 +0530

    pin code feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,6 +15,7 @@ class HelloWorld {
 	System.out.println("Address 2"); 
 	System.out.println("Street"); 
 	System.out.println("Country"); 
+	System.out.println("Pincode"); 
 	System.out.println("/*********--------------------*****************/");
     }
 }
